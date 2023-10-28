b88b92909c445f0351422a7dd05e7132cfef2019
NarayanaKedilaya <147658088+NarayanaKedilaya@users.noreply.github.com>
2023-10-28 19:43:45 +0530

    Add files via upload

---
+++ S1.java
@@ -0,0 +1,20 @@
+package com.sangamone;
+
+public class S1 {
+
+	public static void main(String[] args) {
+		int a=8;
+		double b=16.1;
+		double c=a+b;
+	
+		System.out.println("c="+  c);
+		
+		a=25;
+		b=50;
+		c=a-b;
+		
+		System.out.println(c);
+
+	}
+
+}
---
+++ S2.java
@@ -0,0 +1,41 @@
+package com.sangamone;
+
+public class S2 {
+
+	public static void main(String[] args) {
+		int a;
+		int b;
+		int c;
+		/*a=3;
+		b=1;
+		c=a*b;
+		System.out.println(a + "*" +b+ "=" +c);
+		b=2;
+		c=a*b;
+		System.out.println(a + "*" +b+ "=" +c);
+		b=3;
+		c=a*b;
+		System.out.println(a+ "*" +b+ "=" +c);*/
+		
+	
+		for( b=1;b<=10;b++) {
+		   // System.out.println(b);
+		    a=3;
+		    c=a*b;
+		    System.out.println(a + "*" +b+ "=" +c);
+		    
+		}
+		System.out.println();
+
+		for( b=1;b<=10;b++) {
+		   // System.out.println(b);
+		    a=4;
+		    c=a*b;
+		    System.out.println(a + "*" +b+ "=" +c);
+		    
+		}
+	
+
+	}
+
+}

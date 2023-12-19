a65aae2a03e70e6bbc2c06b6e3ebc09b0bb6c9e1
NarayanaKedilaya <147658088+NarayanaKedilaya@users.noreply.github.com>
2023-12-19 11:13:06 +0530

    Add files via upload

---
+++ Math.java
@@ -0,0 +1,17 @@
+package com.sangamone;
+
+public class Math {
+
+	public static void main(String[] args) {
+	int count=100;
+	for (int j=2;j<count;j++) {
+		for(int i=0;i<(count/j)+1;i++) {
+			System.out.print(j*i +" ");
+			
+		}
+		System.out.println();
+	}
+
+	}
+
+}
